1352c3779af0ce74abdc0ca544d50e52a1dadcf3
kyungin <kyungin@tmon.co.kr>
2022-09-19 09:52:47 +0900

    change package

--- src/main/java/com/chloestack/algorithms/datastructures/binarysearchtree/questions/leedcode/IsValidBSTUsingLinkedList.java
+++ src/main/java/com/chloestack/algorithms/datastructures/binarysearchtree/questions/leedcode/IsValidBSTUsingLinkedList.java
@@ -1,4 +1,4 @@
-package com.chloestack.algorithms.datastructures.binarysearchtree.questions;
+package com.chloestack.algorithms.datastructures.binarysearchtree.questions.leedcode;
 
 import java.util.Deque;
 import java.util.LinkedList;
