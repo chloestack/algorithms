48ef128094d88da034563b102d21966e67bef671
chloestack <kyungin2@naver.com>
2022-09-30 17:43:14 +0900

    leet 101

---
+++ src/test/java/com/chloestack/algorithms/datastructures/binarysearchtree/DFS/leetcode/IsSymmetricTest.java
@@ -0,0 +1,26 @@
+package com.chloestack.algorithms.datastructures.binarysearchtree.DFS.leetcode;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IsSymmetricTest {
+
+    @Test
+    void testIsSymmetric() {
+
+        TreeNode test1 = new TreeNode(3);
+        TreeNode test2 = new TreeNode(1);
+        TreeNode test3 = new TreeNode(5);
+
+        TreeNode compareA = new TreeNode(2, test1, test2);
+        TreeNode compareB = new TreeNode(2, test2, test1);
+
+        TreeNode target = new TreeNode(7, compareA, compareB);
+
+        IsSymmetric sym = new IsSymmetric();
+        System.out.println(sym.isSymmetric(target));
+        System.out.println(target.printNodes());
+    }
+
+}
