522f9218b2a69747e4198a7cb7ea8ec7625ea2df
chloestack <kyungin2@naver.com>
2022-10-01 11:54:37 +0900

    leat 104

---
+++ src/main/java/com/chloestack/algorithms/datastructures/binarysearchtree/DFS/leetcode/MaximumDepth.java
@@ -0,0 +1,36 @@
+package com.chloestack.algorithms.datastructures.binarysearchtree.DFS.leetcode;
+
+/**
+ * Leet 104.
+ */
+public class MaximumDepth {
+
+    public int maxDepth(TreeNode root ) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        return countDepth(root, 1);
+    }
+
+    private int countDepth(TreeNode root, int sum){
+        if (root.left == null && root.right == null) {
+            return sum;
+        }
+
+        if (root.left != null) {
+            if (root.right != null) {
+                int l = countDepth(root.left, sum + 1);
+                int r = countDepth(root.right, sum + 1);
+                return l >= r ?  l : r;
+            } else {
+                return countDepth(root.left, sum + 1);
+            }
+        }
+
+        return countDepth(root.right, sum + 1);
+
+    }
+
+}
---
+++ src/test/java/com/chloestack/algorithms/datastructures/binarysearchtree/DFS/leetcode/MaximumDepthTest.java
@@ -0,0 +1,33 @@
+package com.chloestack.algorithms.datastructures.binarysearchtree.DFS.leetcode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaximumDepthTest {
+
+
+    @Test
+    void maxDepth() {
+
+        TreeNode test4 = new TreeNode(9);
+        TreeNode test3 = new TreeNode(15);
+        TreeNode test2 = new TreeNode(7);
+        TreeNode test1 = new TreeNode(20, test3, test2);
+        TreeNode test0 = new TreeNode(3, test4, test1);
+
+        System.out.println(test0.printNodes());
+        MaximumDepth md = new MaximumDepth();
+        assertEquals(3, md.maxDepth(test0));
+
+        test1 = new TreeNode(2);
+        test0 = new TreeNode(1, null, test1);
+
+        System.out.println(test0.printNodes());
+        assertEquals(2, md.maxDepth(test0));
+        System.out.println(md.maxDepth(test0));
+    }
+
+
+}
